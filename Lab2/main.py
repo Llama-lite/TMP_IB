3a3356e3d6688fc7df3957e5833d2c31bcc08852
Llama-lite <drb028@yandex.ru>
2025-04-15 00:18:09 +0700

    lab2 final

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,45 +2,198 @@ import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
                              QTableView, QPushButton, QLineEdit, QDateEdit, QSpinBox,
                              QLabel, QMessageBox, QFileDialog, QComboBox)
-from PyQt6.QtCore import QDate, Qt
-from PyQt6.QtGui import QStandardItemModel, QStandardItem
+from PyQt6.QtCore import QDate, Qt, QAbstractTableModel
 from Cake import Cake
 from Cup import Cup
 from Belt import Belt
 import datetime
 
+class ProductManager:
+    def __init__(self):
+        self.products = []
+    
+    def add_product(self, product):
+        self.products.append(product)
+    
+    def delete_product(self, index):
+        if 0 <= index < len(self.products):
+            del self.products[index]
+    
+    def clear_products(self):
+        self.products = []
+    
+    def get_products(self):
+        return self.products.copy()
 
-# Main window class
+class ProductTableModel(QAbstractTableModel):
+    def __init__(self, product_manager, parent=None):
+        super().__init__(parent)
+        self.product_manager = product_manager
+        self.headers = ["Supply Date", "Name", "Amount", "Special Attribute"]
+    
+    def columnCount(self, parent=None):
+        return len(self.headers)
+    
+    def rowCount(self, parent=None):
+        return len(self.product_manager.products)
+    
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if not index.isValid() or role != Qt.ItemDataRole.DisplayRole:
+            return None
+        
+        product = self.product_manager.products[index.row()]
+        
+        if index.column() == 0:
+            return str(product.supplyDate)
+        elif index.column() == 1:
+            return product.name
+        elif index.column() == 2:
+            return str(product.ammount)
+        elif index.column() == 3:
+            if isinstance(product, Belt):
+                return str(product.metal)
+            elif isinstance(product, Cake):
+                return str(product.height)
+            elif isinstance(product, Cup):
+                return str(product.volume)
+        return None
+    
+    def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
+        if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Horizontal:
+            return self.headers[section]
+        return None
+
+class ProductFormManager:
+    """Manages the dynamic form fields for different product types"""
+    def __init__(self, special_layout):
+        self.special_layout = special_layout
+        self.special_fields = []
+    
+    def update_form_fields(self, product_type):
+        # Clear existing fields
+        self.clear_fields()
+        
+        if product_type == "Belt":
+            self.create_belt_fields()
+        elif product_type == "Cake":
+            self.create_cake_fields()
+        elif product_type == "Cup":
+            self.create_cup_fields()
+    
+    def clear_fields(self):
+        for i in range(self.special_layout.count()):
+            for j in range(self.special_layout.itemAt(i).layout().count()):
+                self.special_layout.itemAt(i).layout().itemAt(j).widget().deleteLater()
+            self.special_layout.itemAt(i).layout().deleteLater()
+        self.special_fields = []
+    
+    def create_belt_fields(self):
+        metal_layout = QVBoxLayout()
+        metal_layout.addWidget(QLabel("Metal?"))
+        metal_select = QComboBox()
+        metal_select.addItems(["True", "False"])
+        metal_layout.addWidget(metal_select)
+        self.special_layout.addLayout(metal_layout)
+        self.special_fields = [metal_select]
+    
+    def create_cake_fields(self):
+        cake_layout = QVBoxLayout()
+        cake_layout.addWidget(QLabel("Height"))
+        cake_height = QSpinBox()
+        cake_height.setMinimum(5)
+        cake_height.setMaximum(1000)
+        cake_layout.addWidget(cake_height)
+        self.special_layout.addLayout(cake_layout)
+        self.special_fields = [cake_height]
+    
+    def create_cup_fields(self):
+        cup_layout = QVBoxLayout()
+        cup_layout.addWidget(QLabel("Volume"))
+        cup_volume = QSpinBox()
+        cup_volume.setMinimum(30)
+        cup_volume.setMaximum(3000)
+        cup_layout.addWidget(cup_volume)
+        self.special_layout.addLayout(cup_layout)
+        self.special_fields = [cup_volume]
+    
+    def get_special_field_value(self):
+        if self.special_fields:
+            if isinstance(self.special_fields[0], QComboBox):
+                return bool(self.special_fields[0].currentText())
+            elif isinstance(self.special_fields[0], QSpinBox):
+                return self.special_fields[0].value()
+        return None
+
+class ProductFileHandler:
+    @staticmethod
+    def save_products(products, filename):
+        with open(filename, 'w') as file:
+            for product in products:
+                file.write(str(product)+"\n")
+    
+    @staticmethod
+    def load_products(filename):
+        products = []
+        with open(filename, 'r') as file:
+            for line in file:
+                supply_type, values = line.strip().split("(")
+                values = values[0:-1].split(", ")
+                
+                if supply_type == "Belt":
+                    products.append(Belt(
+                        supplyDate=datetime.datetime.strptime(values[0], "%d.%m.%Y"),
+                        name=values[1][1:-1],
+                        ammount=int(values[2]),
+                        metal=(values[3].lower() == "true")
+                    ))
+                elif supply_type == "Cake":
+                    products.append(Cake(
+                        supplyDate=datetime.datetime.strptime(values[0], "%d.%m.%Y"),
+                        name=values[1][1:-1],
+                        ammount=int(values[2]),
+                        height=int(values[3])
+                    ))
+                elif supply_type == "Cup":
+                    products.append(Cup(
+                        supplyDate=datetime.datetime.strptime(values[0], "%d.%m.%Y"),
+                        name=values[1][1:-1],
+                        ammount=int(values[2]),
+                        volume=int(values[3])
+                    ))
+        return products
+
 class ProductWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Product supply")
         self.setGeometry(100, 100, 800, 600)
         
-        self.products = []  # For Product children objects
-        self.special_fields = [] # For special class fields
+        # Initialize components
+        self.product_manager = ProductManager()
+        self.file_handler = ProductFileHandler()
         
-        # Create central widget and layout
+        # Create UI
+        self.init_ui()
+    
+    def init_ui(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
         
-        # Create table view and model
+        # Create table view
         self.table_view = QTableView()
-        self.model = QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(["Supply Date", "Name", "Amount", '*'])
-        self.table_view.setModel(self.model)
+        self.table_model = ProductTableModel(self.product_manager)
+        self.table_view.setModel(self.table_model)
         layout.addWidget(self.table_view)
         
-        # Create form for adding new products
+        # Create form
         form_layout = QHBoxLayout()
         
-        # Type input
+        # Type selection
         type_layout = QVBoxLayout()
         type_layout.addWidget(QLabel("Product type:"))
         self.type_select = QComboBox()
         self.type_select.addItems(["Belt", "Cake", "Cup"])
-        self.type_select.activated.connect(self.selected_type_changed)
         type_layout.addWidget(self.type_select)
         form_layout.addLayout(type_layout)
         
@@ -67,20 +220,16 @@ class ProductWindow(QMainWindow):
         amount_layout.addWidget(self.amount_edit)
         form_layout.addLayout(amount_layout)
         
-        # Container for special class fields
+        # Special fields container
         self.special_layout = QHBoxLayout()
         form_layout.addLayout(self.special_layout)
         
-        # Container Belt fields (as it is first selected)
-        metal_layout = QVBoxLayout()
-        metal_layout.addWidget(QLabel("Metal?"))
-        metal_select = QComboBox()
-        metal_select.addItems(["True", "False"])
-        metal_layout.addWidget(metal_select)
-        self.special_layout.addLayout(metal_layout)
-        self.special_fields = [metal_select]
+        # Initialize form manager
+        self.form_manager = ProductFormManager(self.special_layout)
+        self.type_select.activated.connect(self.on_type_changed)
+        self.on_type_changed()  # Initialize with default fields
         
-        # Add button
+        # Add Product button
         self.add_button = QPushButton("Add Product")
         self.add_button.clicked.connect(self.add_product)
         form_layout.addWidget(self.add_button)
@@ -107,35 +256,39 @@ class ProductWindow(QMainWindow):
         
         layout.addLayout(button_layout)
     
+    def on_type_changed(self):
+        self.form_manager.update_form_fields(self.type_select.currentText())
     
-    # Add new product to the list and update table
     def add_product(self):
-        supplyDate = datetime.datetime.combine(self.date_edit.date().toPyDate(), datetime.datetime.min.time())
         name = self.name_edit.text().strip()
-        ammount = self.amount_edit.value()
-        
         if not name:
             QMessageBox.warning(self, "Warning", "Product name cannot be empty!")
             return
         
-        if self.type_select.currentText() == "Belt":
-            self.products.append(Belt(supplyDate, name, ammount, bool(self.special_fields[0].currentText())))
-        elif self.type_select.currentText() == "Cake":
-            self.products.append(Cake(supplyDate, name, ammount, bool(self.special_fields[0].value())))
-        else:
-            self.products.append(Cup(supplyDate, name, ammount, bool(self.special_fields[0].value())))
+        supply_date = datetime.datetime.combine(
+            self.date_edit.date().toPyDate(),
+            datetime.datetime.min.time()
+        )
+        amount = self.amount_edit.value()
+        special_value = self.form_manager.get_special_field_value()
         
-        self.update_table()
+        product_type = self.type_select.currentText()
+        if product_type == "Belt":
+            product = Belt(supply_date, name, amount, special_value)
+        elif product_type == "Cake":
+            product = Cake(supply_date, name, amount, special_value)
+        else:
+            product = Cup(supply_date, name, amount, special_value)
+        
+        self.product_manager.add_product(product)
+        self.table_model.layoutChanged.emit()
     
-    
-    # Delete selected product from table and file
     def delete_product(self):
         selected = self.table_view.currentIndex()
         if not selected.isValid():
             QMessageBox.warning(self, "Warning", "Please select a product to delete!")
             return
         
-        # Confirm deletion
         reply = QMessageBox.question(
             self, "Confirm Delete", 
             "Are you sure you want to delete this product?",
@@ -143,102 +296,33 @@ class ProductWindow(QMainWindow):
         )
         
         if reply == QMessageBox.StandardButton.Yes:
-            row = selected.row()
-            del self.products[row]
-            self.update_table()
+            self.product_manager.delete_product(selected.row())
+            self.table_model.layoutChanged.emit()
     
-    
-    # Change UI based on selected product type
-    def selected_type_changed(self):
-        selected = self.type_select.currentText()
-        
-        for i in range(self.special_layout.count()):
-            container = self.special_layout.itemAt(i)
-            for j in range(container.count()):
-                container.itemAt(j).widget().deleteLater()
-            container.layout().deleteLater()
-        
-        if selected == "Belt":
-            metal_layout = QVBoxLayout()
-            metal_layout.addWidget(QLabel("Metal?"))
-            metal_select = QComboBox()
-            metal_select.addItems(["True", "False"])
-            metal_layout.addWidget(metal_select)
-            self.special_layout.addLayout(metal_layout)
-            self.special_fields = [metal_select]
-        elif selected == "Cake":
-            cake_layout = QVBoxLayout()
-            cake_layout.addWidget(QLabel("Height"))
-            cake_height = QSpinBox()
-            cake_height.setMinimum(5)
-            cake_height.setMaximum(1000)
-            cake_layout.addWidget(cake_height)
-            self.special_layout.addLayout(cake_layout)
-            self.special_fields = [cake_height]
-        elif selected == "Cup":
-            cup_layout = QVBoxLayout()
-            cup_layout.addWidget(QLabel("Volume"))
-            cup_volume = QSpinBox()
-            cup_volume.setMinimum(30)
-            cup_volume.setMaximum(3000)
-            cup_layout.addWidget(cup_volume)
-            self.special_layout.addLayout(cup_layout)
-            self.special_fields = [cup_volume]
-        ...
-    
-    # Update visual grid table items
-    def update_table(self):
-        self.model.clear()
-        self.model.setHorizontalHeaderLabels(["Supply Date", "Name", "Amount", "*"])
-        
-        for product in self.products:
-            date_item = QStandardItem(str(product.supplyDate))
-            name_item = QStandardItem(product.name)
-            amount_item = QStandardItem(str(product.ammount))
-            
-            if isinstance(product, Belt):
-                metal_item = QStandardItem(str(product.metal))
-                self.model.appendRow([date_item, name_item, amount_item, metal_item])
-            elif isinstance(product, Cake):
-                height_item = QStandardItem(str(product.height))
-                self.model.appendRow([date_item, name_item, amount_item, height_item])
-            elif isinstance(product, Cup):
-                volume_item = QStandardItem(str(product.volume))
-                self.model.appendRow([date_item, name_item, amount_item, volume_item])
-    
-    
-    # Save data to selected file
     def save_products(self):
-        filename, _ = QFileDialog.getSaveFileName(None, "Save File", ".", "Text Files (*.txt);;All Files (*)")
+        filename, _ = QFileDialog.getSaveFileName(
+            None, "Save File", ".", "Text Files (*.txt);;All Files (*)"
+        )
         if filename:
-            with open(filename, 'w') as file:
-                for product in self.products:
-                    file.write(str(product)+"\n")
+            self.file_handler.save_products(
+                self.product_manager.get_products(),
+                filename
+            )
     
-    
-    # Load data from file
     def load_products(self):
-        filename, _ = QFileDialog.getOpenFileName(None, "Save File", ".", "Text Files (*.txt);;All Files (*)")
+        filename, _ = QFileDialog.getOpenFileName(
+            None, "Open File", ".", "Text Files (*.txt);;All Files (*)"
+        )
         if filename:
-            products = []
-            with open(filename, 'r') as file:
-                for line in file:
-                    supply_type, values = line.strip().split("(")
-                    values = values[0:-1].split(", ")
-                    
-                    if supply_type == "Belt":
-                        products.append(Belt(supplyDate=datetime.datetime.strptime(values[0], "%d.%m.%Y"), name=values[1][1:-1], ammount=int(values[2]), metal=(values[3].lower() == "true")))
-                    
-                    elif supply_type == "Cake":
-                        products.append(Cake(supplyDate=datetime.datetime.strptime(values[0], "%d.%m.%Y"), name=values[1][1:-1], ammount=int(values[2]), height=int(values[3])))
-                    
-                    elif supply_type == "Cup":
-                        products.append(Cup(supplyDate=datetime.datetime.strptime(values[0], "%d.%m.%Y"), name=values[1][1:-1], ammount=int(values[2]), volume=int(values[3])))
-            
-            self.products = products
-            self.update_table()
-            QMessageBox.information(self, "Success", "Data loaded successfully!")
-
+            try:
+                products = self.file_handler.load_products(filename)
+                self.product_manager.clear_products()
+                for product in products:
+                    self.product_manager.add_product(product)
+                self.table_model.layoutChanged.emit()
+                QMessageBox.information(self, "Success", "Data loaded successfully!")
+            except Exception as e:
+                QMessageBox.critical(self, "Error", f"Failed to load file: {str(e)}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
